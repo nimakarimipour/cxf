00625cc7a8601979277613cf46e1ec43478416d7
Nima Karimipour <karimipour.nima@gmail.com>
2024-03-27 08:42:20 -0700

    update config

--- annotator-command.sh
+++ annotator-command.sh
@@ -4,4 +4,4 @@ poly=true
 cf_version="3.42.0"
 ucrt_version="0.1-alpha-3-SNAPSHOT"
 rm -rvf core/target > /dev/null
-CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -pl core -Drat.skip=true
+CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -pl core -Drat.skip=true -Dcheckstyle.skip=true
