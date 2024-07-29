47ec2f1e713f28f143697f8be43c6bdeb31bb828
Kanak Das <kdas006@ucr.edu>
2024-07-29 15:39:37 -0700

    no opt

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -47,7 +47,7 @@ def run_annotator():
     # Uncomment to disable cache
     # commands += ['-dc']
     # Uncomment to disable outer loop
-#     commands += ['-dlo']
+    commands += ['-dlo']
     # Uncomment to disable parallel processing
     commands += ['--disable-parallel-processing']
     subprocess.call(commands)
